737c9c79bfa76669b489364f4f011ee4377d366c
souph1s <sophiamperez@hotmail.com>
2024-03-19 01:25:28 -0300

    feat: styles for modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -13,13 +14,7 @@ export function Header() {
             <NewTransactionButton>New Transaction</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay />
-            <Dialog.Content>
-              <Dialog.Title>New Transaction</Dialog.Title>
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,27 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { CloseButton, Content, Overlay } from "./styles";
+import { X } from "phosphor-react";
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <Content>
+        <Dialog.Title>New Transaction</Dialog.Title>
+
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
+        <form>
+          <input type="text" placeholder="Description" required />
+          <input type="number" placeholder="Price" required />
+          <input type="text" placeholder="Category" required />
+
+          <button type="submit">Add</button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  );
+}
