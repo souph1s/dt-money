e0eefe843dab647981cf45707a98f6d5ba088689
souph1s <sophiamperez@hotmail.com>
2024-04-01 23:37:16 -0300

    formatting values

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,6 +2,7 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
 import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormater } from "../../utils/formatter";
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
@@ -31,14 +32,14 @@ export function Summary() {
           <span>Earnings</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-        <strong>{summary.income}</strong>
+        <strong>{priceFormater.format(summary.income)}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Spent</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormater.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -46,7 +47,7 @@ export function Summary() {
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
-        <strong>{summary.total}</strong>
+        <strong>{priceFormater.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
---
+++ src/utils/formatter.ts
@@ -0,0 +1,6 @@
+export const dateFormatter = new Intl.DateTimeFormat("pt-br");
+
+export const priceFormater = new Intl.NumberFormat("pt-br", {
+  style: "currency",
+  currency: "BRL",
+});
